5b70fcd0af8e25add2f0788df20b4a946309fd56
Marco Maurer <marcomaurer@gmail.com>
2022-08-19 19:28:27 -0300

    adiciona resolucao de exercicios do 1 ao 4

--- Modulo1/bloco-04-JavaScript-DOM-Eventos-WebStore/dia-02-arrays-e-loop-for/script.js
+++ Modulo1/bloco-04-JavaScript-DOM-Eventos-WebStore/dia-02-arrays-e-loop-for/script.js
@@ -56,11 +56,64 @@
 // }
 
 
-let counter = 0;
+// let counter = 0;
 
-while(counter !== 5) {
-  counter += 1;
-}
+// while(counter !== 5) {
+//   counter += 1;
+// }
+
+// Exercicios 4.2 - o array abaixo é para o exercicio 1 ao 7:/
+
+// exercicio 01:/
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers[0])
+// console.log(numbers[1])
+// console.log(numbers[2])
+// console.log(numbers[3])
+// console.log(numbers[4])
+// console.log(numbers[5])
+// console.log(numbers[6])
+// console.log(numbers[7])
+// console.log(numbers[8])
+// console.log(numbers[9])
+
+// Exercicio 02:/
+
+// let resultado = 0
+
+//   for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index];
+// }
+// console.log(resultado);
+
+
+// Exercicio 03 e 04: /
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     resultado += numbers[index] /= 10;   
+//     }
+
+// if ( resultado > 20) {
+//         console.log ("Valor maior que 20");
+// }
+// else {
+//     console.log ("Valor menor ou igual a 20");
+// }
+
+// exercicio 05:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValorDoArray = [0]
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index])
+    for (let numeroMaior = numbers[index]; maiorValorDoArray < numbers.length; index +1) {
+        console.log(maiorValorDoArray)
+    }
+    }
 
 
 
