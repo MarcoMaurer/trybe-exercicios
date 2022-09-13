ec0e09f67487c77af5668362386ea761240ba4ea
Marco Maurer <marcomaurer@gmail.com>
2022-09-13 10:57:14 -0300

    adiciona js para name e e-mail na mesma function

--- Modulo1/bloco-06-HTML-CSS/script.js
+++ Modulo1/bloco-06-HTML-CSS/script.js
@@ -1,13 +1,19 @@
 const botaoSubmit = document.querySelector("#buton1");
-let dadosArmazenados = document.querySelector("#myname","#myoptions", "#escrevaaqui", "#data", "#agree1", "#agree2");
+let dadosArmazenados = document.querySelector("#myname");
 const result = document.querySelector("#resultado");
 
 function getResult (event) {
     event.preventDefault();
-    console.log(dadosArmazenados.value);
+    dadosArmazenados.value = "Nome completo: " + dadosArmazenados.value;
     result.innerHTML = dadosArmazenados.value;
+    let escondeHidden = document.getElementsByClassName("hidden")[0];
+    escondeHidden.classList.remove("hidden");
+    dadosArmazenados.value = "";
+
+    const atribuiEmail = document.getElementById("myemail").value;
+    result.innerHTML = result.innerHTML + " E-mail: " + atribuiEmail;
+    atribuiEmail.value = "";
 }
 
 botaoSubmit.addEventListener('click', getResult);
 
-// + "#myoptions" + "#escrevaaqui" + "#data" + "#agree1" + "#agree2"
